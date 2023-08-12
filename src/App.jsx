55b7b20dbbc92c2a120f4065bde9432fc71a542e
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-08-12 18:44:31 -0300

    Add: responsive

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,16 @@ export function App () {
   }
 
   return (
-    <main className='font-serif p-10 w-full h-max'>
+    <main className='font-serif lg:p-10 w-screen h-max sm:px-1 px-2'>
       <section className='flex justify-center'>
-        <h1 className='text-6xl mt-5 mb-10 flex'>CatFac<span className='block text-7xl rotate-12'>t</span></h1>
+        <h1 className='text-6xl mt-5 mb-10 flex text-red-900'>CatFac<span className='block text-7xl rotate-12'>t</span></h1>
         <img className='w-7' src='cat_fact.svg' />
       </section>
       <button className='bg-red-200 px-3 py-2 text-xl rounded-md mb-10 mt-5 shadow-md shadow-gray-200 hover:bg-orange-100 hover:animate-pulse sm:hover:animate-none md:hover:animate-none' onClick={handleClick}>Get new fact </button>
-      <section className='flex gap-10 mx-auto my-14 sm:flex-col sm:justify-center sm:place-items-center mb:flex-col mb:justify-center mb:mx-2 mb:w-72 mb:text-2xl'>
-        {fact && <p className='mb-10 max-w-lg text-4xl max-w-lg md:text-3xl'>{fact}</p>}
+      <section className='lg:flex lg:flex-row sm:flex-col sm:justify-center gap-10 mt-10 mx-4'>
+        {fact && <p className='mb-10 max-w-lg text-4xl max-w-lg'>{fact}</p>}
         <img
-          className='rounded-md shadow-lg shadow-gray-400 max-w-[400px] max-h-[600px] md:w-96'
+          className='rounded-md shadow-lg shadow-gray-400 max-h-[500px] object-contain'
           src={imageUrl}
           alt={`Imagen extraida de Api que contiene las tres primeras palabras de ${fact}`}
         />
